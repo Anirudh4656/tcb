a7e45227e0f0eff1abfcb3de7cf9163c2def0453
ANIRUDH <anirudh4656@gmail.com>
2023-05-07 21:57:29 +0530

    last commit

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,8 @@
+// export const environment = {
+//   production: false,
+//   ADMIN_API_URL: 'http://localhost:3000/api/admin',
+//   PUBLIC_API_URL: 'http://localhost:3000/api/public'
+// };
 export const environment = {
   production: false,
   ADMIN_API_URL: 'https://tcb.onrender.com/api/admin',
