aef30d00a9787bf0abf5bce3af843125b1540388
ANIRUDH <anirudh4656@gmail.com>
2023-05-14 01:52:33 +0530

    commits

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ import { PostComponent } from '@page/post/post.component';
 import { SearchBoxComponent } from '@page/search-box/search-box.component';
 
 const routes: Routes = [
-  { path: '', component: IndexComponent },
-  { path: '/home', component: IndexComponent },
-  { path: '/:id', component: PostComponent },
-  { path: '/category/:category/:sub_category',component: CategoryPostComponent },
-  { path: '/search/:keyword', component: SearchBoxComponent },
-  { path: '/admin', loadChildren: () => import('./pages/admin/admin.module').then(getModule => getModule.AdminModule) }
+  { path: 'https://thechoicebox.netlify.app/', component: IndexComponent },
+  { path: 'https://thechoicebox.netlify.app/home', component: IndexComponent },
+  { path: 'https://thechoicebox.netlify.app/:id', component: PostComponent },
+  { path: 'https://thechoicebox.netlify.app/category/:category/:sub_category',component: CategoryPostComponent },
+  { path: 'https://thechoicebox.netlify.app/search/:keyword', component: SearchBoxComponent },
+  { path: 'https://thechoicebox.netlify.app/admin', loadChildren: () => import('./pages/admin/admin.module').then(getModule => getModule.AdminModule) }
 ];
 
 @NgModule({
