427f82023a2654605605f3b626eb5d4e730347d8
ANIRUDH <anirudh4656@gmail.com>
2023-05-14 12:08:07 +0530

    commits

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { SearchBoxComponent } from '@page/search-box/search-box.component';
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: '/home', component: IndexComponent },
-  { path: 'https://thechoicebox.netlify.app/:id', component: PostComponent },
-  { path: 'https://thechoicebox.netlify.app/category/:category/:sub_category',component: CategoryPostComponent },
+  { path: '/post/:id', component: PostComponent },
+  { path: '/category/:category/:sub_category',component: CategoryPostComponent },
   { path: '/search/:keyword', component: SearchBoxComponent },
   { path: '/admin', loadChildren: () => import('./pages/admin/admin.module').then(getModule => getModule.AdminModule) }
 ];
